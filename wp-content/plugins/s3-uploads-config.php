e6d19deffe812f3cb591c6478c96fb2bd649504a
Jeremiah Prasetyo <jere.prasetyo@gmail.com>
2024-12-29 03:59:01 +0000

    changer-port

--- wp-content/plugins/s3-uploads-config.php
+++ wp-content/plugins/s3-uploads-config.php
@@ -1,6 +1,6 @@
 <?php
 add_filter( 's3_uploads_s3_client_params', function( $params ) {
-	$params['endpoint'] = 'http://minio:9000';
+	$params['endpoint'] = 'http://192.168.60.231:30009';
 	$params['use_path_style_endpoint'] = true;
 	return $params;
 } );
